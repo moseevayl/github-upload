1eb4df2dfc14351a31485c9548313edf9262cc73
moseevayl <super.xblegje2014@yandex.ru>
2022-06-18 17:16:33 +0500

    ffgf
    ???

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hell o World!");
+        }
+    }
+}
